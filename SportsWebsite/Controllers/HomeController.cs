02c68b94daf6981a3b07b0285186f1ef836b823b
gaganpal <gaganpal9356@gmail.com>
2017-04-12 00:36:44 -0400

    initial commit

---
+++ SportsWebsite/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SportsWebsite.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetView()
+        {
+            return View("HomeView");
+        }
+
+        public string GetString()
+        {
+            return "Hey Adnan";
+        }
+        public ActionResult Football()
+        {
+            return View("FootballMain");
+        }
+    }
+}
