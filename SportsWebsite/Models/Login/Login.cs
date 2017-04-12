64c0ea40432e074e9c51cbaceb653fe51b862796
gaganpal <gaganpal9356@gmail.com>
2017-04-12 01:43:36 -0400

    login model added

---
+++ SportsWebsite/Models/Login/Login.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SportsWebsite.Models.Login
+{
+    public class Login
+    {
+        [Required(ErrorMessage = "Username is required!!")]
+            public string Username { get; set; }
+        [Required(ErrorMessage = "Password is required!!")]
+        public string Password { get; set; }
+        public string Message { get; set; }
+        public string Status { get; set; }
+    }
+}
